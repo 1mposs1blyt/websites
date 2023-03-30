ae1e81a51521f3f6cb70a127fa67d190e538ee4f
Yarik <yrdy>
2023-03-30 15:44:01 +0700

    первый комит

--- websites/website1/app.js
+++ websites/website1/app.js
@@ -1,10 +1,39 @@
-const express = require('express');
-const app = express();  
+const express = require("express");
+const nunjucks = require("nunjucks");
+const bodyParser = require("body-parser");
+const app = express();
+const host = "127.0.0.1";
+const port = "4000";
 
-app.get('/', function(req, res){
-    res.send('Welcome to the club buddy')
-})
 
+nunjucks.configure(__dirname + "/views", {
+  autoescape: true,
+  express: app,
+});
+
+//Dodiparser parse
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//use static files in folders
+app.use(express.static("public/static"));
+
+//index.html
+app.get("/", (req, res) => {
+  res.render("index.html");
+});
+
+//places.html
+app.get("/places", (req, res) => {
+  res.render("places.html");
+});
+
+//post
+app.post("/post", (req, res) => {
+  console.log(req.body);
+});
+
+//listen
 app.listen(port, host, function () {
-    console.log(`Server listens http://${host}:${port}`)
-})
+  console.log(`Server listens http://${host}:${port}`);
+});
